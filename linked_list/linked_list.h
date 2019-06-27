0d5d1bea173bff664da8343f4f5eec4dafbe9526
max <29157840+mmhanson@users.noreply.github.com>
2019-06-27 11:25:07 -0600

    Redoing linked list... Done rewriting linked list docs.

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -67,8 +67,8 @@ struct _LListNodeTag
 
 /*
  * Add @new_node after @node in the list.
- * Every case complexity: O(1).
- * No need to initialize @new_node.
+ * O(1) complexity. @new_node doesnt need to be initialized, its members will be
+ * overwritten anyway.
  *
  * @node: Add @new_node after this node in the list.
  * @new_node: Add this node after @node in the list.
@@ -98,8 +98,8 @@ static inline void llist_add_after(LListNode *node, LListNode *new_node,
 
 /*
  * Add @new_node before @node in the list.
- * Every case complexity: O(1).
- * No need to initialize @new_node.
+ * O(1) complexity. @new_node doesnt need to be initialized, its members will be
+ * overwritten anyway.
  *
  * @node: Add @new_node before this node in the list.
  * @new_node: Add this node before @node in the list.
@@ -129,9 +129,7 @@ static inline void llist_add_before(LListNode *node, LListNode *new_node,
 
 /*
  * Remove (unlink) @node from the list.
- * Does not deallocate the node after removal. This must be done by the client.
- * Every case complexity: O(1).
- * TODO doc that @node will still point into the list afterwards. Diagram?
+ * O(1) complexity. Caution! @node will not be deallocated.
  *
  * @node: The node to be removed from the list.
  */
@@ -139,13 +137,16 @@ static inline void llist_remove(LListNode *node)
 {
     node->prev->next = node->next;
     node->next->prev = node->prev;
+    node->next = 0;
+    node->prev = 0;
 }
 
 /*
  * Replace @old_node with @new_node.
- * Does not deallocate @old_node. This must be done by the client.
- * Every case complexity: O(1).
- * TODO doc that @old_node will point into the list afterwards. Diagram?
+ * O(1) complexity. Caution! @old_node is not deallocated.
+ *
+ * @old_node: The node to be replaced by @new_node.
+ * @new_node: The node to replace @old_node with.
  */
 static inline void llist_replace(LListNode *old_node, LListNode *new_node)
 {
@@ -156,17 +157,16 @@ static inline void llist_replace(LListNode *old_node, LListNode *new_node)
     /* Unlink old node. */
     old_node->prev->next = new_node;
     old_node->next->prev = new_node;
+    old_node->next = 0;
+    old_node->prev = 0;
 }
 
 /*
  * Swap @node_a and @node_b in the list.
- * Every case complexity: O(1).
- * 
- * If:
- *   - TODO
- * Then:
- *   - @node_a will take @node_b's previous position in the list.
- *   - @node_b will take @node_a's previous position in the list.
+ * O(1) complexity.
+ *
+ * @node_a: Will take @node_b's previous position in the list.
+ * @node_b: Will take @node_a's previous position in the list.
  */
 static inline void llist_swap(LListNode *node_a, LListNode *node_b)
 {
