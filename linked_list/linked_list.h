fedff75849f5412a5abfe09c6aaafbc1454b144a
max <29157840+mmhanson@users.noreply.github.com>
2019-06-27 10:31:30 -0600

    Redoing linked list... Finished redoing operations. Now redoing documentation>

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,7 +1,7 @@
 /*
- * A basic circular, doubly linked list.
+ * A basic doubly linked list.
  *
- * USAGE
+ * === How To Use ===
  *  - TODO add info about embedding LListNode into structs.
  *
  * Written by Max Hanson, June 2019.
@@ -19,15 +19,14 @@ struct _LListNodeTag
 };
 
 /*
- * Initialize a linked list node to its default values (circular to itself).
+ * Initialize a linked list node to its default values.
  */
-#define LLIST_INIT(container, head_var_name) \
-    { container, (&head_var_name), (&head_var_name) }
+#define LLIST_INIT(container, head_var_name) {container, 0, 0}
 
 /*
  * Add @new_node after @node in the list.
- *
- * Every case complexity: BIG-THETA(1).
+ * Every case complexity: O(1).
+ * No need to initialize @new_node.
  *
  * @node: Add @new_node after this node in the list.
  * @new_node: Add this node after @node in the list.
@@ -38,16 +37,27 @@ static inline void llist_add_after(LListNode *node, LListNode *new_node,
 {
     new_node->container = new_node_container;
 
-    new_node->next = node->next;
-    new_node->prev = node;
-    node->next->prev = new_node;
-    node->next = new_node;
+    if (node->next == 0)
+    {
+        /* @node is tail. */
+        node->next = new_node;
+        new_node->prev = node;
+        new_node->next = 0;
+    }
+    else
+    {
+        /* @node is intermediate node. */
+        new_node->next = node->next;
+        new_node->prev = node;
+        node->next->prev = new_node;
+        node->next = new_node;
+    }
 }
 
 /*
  * Add @new_node before @node in the list.
- *
- * Every case complexity: BIG-THETA(1).
+ * Every case complexity: O(1).
+ * No need to initialize @new_node.
  *
  * @node: Add @new_node before this node in the list.
  * @new_node: Add this node before @node in the list.
@@ -58,12 +68,105 @@ static inline void llist_add_before(LListNode *node, LListNode *new_node,
 {
     new_node->container = new_node_container;
 
-    new_node->next = node;
-    new_node->prev = node->prev;
-    node->prev->next = new_node;
-    node->prev = new_node;
+    if (node->prev == 0)
+    {
+        /* @node is head. */
+        node->prev = new_node;
+        new_node->next = node;
+        new_node->prev = 0;
+    }
+    else
+    {
+        /* @node is intermediate node. */
+        new_node->next = node;
+        new_node->prev = node->prev;
+        node->prev->next = new_node;
+        node->prev = new_node;
+    }
 }
 
+/*
+ * Remove (unlink) @node from the list.
+ * Does not deallocate the node after removal. This must be done by the client.
+ * Every case complexity: O(1).
+ * TODO doc that @node will still point into the list afterwards. Diagram?
+ *
+ * @node: The node to be removed from the list.
+ */
+static inline void llist_remove(LListNode *node)
+{
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+}
 
+/*
+ * Replace @old_node with @new_node.
+ * Does not deallocate @old_node. This must be done by the client.
+ * Every case complexity: O(1).
+ * TODO doc that @old_node will point into the list afterwards. Diagram?
+ */
+static inline void llist_replace(LListNode *old_node, LListNode *new_node)
+{
+    /* Link in new node. */
+    new_node->next = old_node->next;
+    new_node->prev = old_node->prev;
+
+    /* Unlink old node. */
+    old_node->prev->next = new_node;
+    old_node->next->prev = new_node;
+}
+
+/*
+ * Swap @node_a and @node_b in the list.
+ * Every case complexity: O(1).
+ * 
+ * If:
+ *   - TODO
+ * Then:
+ *   - @node_a will take @node_b's previous position in the list.
+ *   - @node_b will take @node_a's previous position in the list.
+ */
+static inline void llist_swap(LListNode *node_a, LListNode *node_b)
+{
+    LListNode *tmp_prev;
+    LListNode *tmp_next;
+
+    /* Put @node_b in @node_a's spot. */
+    tmp_next = b->next; /* to put a in b's spot */
+    tmp_prev = b->prev;
+    node_b->prev = a->prev;
+    node_b->next = a->next;
+    node_b->next->prev = node_b;
+    node_b->prev->next = node_b;
+
+    /* Put @node_a in @node_b's spot. */
+    node_a->next = tmp_next;
+    node_a->prev = tmp_prev;
+    node_a->next->prev = node_a;
+    node_a->prev->next = node_a;
+}
+
+/*
+ * Iterate through a linked list starting at @start_node.
+ * Iteration starts at (and including) @start_node and goes through the list
+ * including the tail.
+ * 
+ * @start_node: Node to start iteration at.
+ * @cursor: Points to where iteration currently is.
+ */
+#define LLIST_FOR_EACH_AFTER(start_node, cursor) \
+    for (cursor = start_node; cursor != 0; cursor = cursor->next)
+
+/*
+ * Iterate over a linked list between two nodes.
+ * Iteration starts at (and including) @start_node and goes up to (not
+ * including) @end_node.
+ *
+ * @start_node: Node to start iteration at.
+ * @end_node: Node to end iteration before.
+ * @cursor: Points to where iteration currently is.
+ */
+#define LLIST_FOR_EACH_BETWEEN(start_node, end_node, cursor) \
+    for (cursor = start_node; cursor != end_node; cursor = cursor->next)
 
 #endif
