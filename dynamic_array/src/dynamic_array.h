535c9756cf585531816a8864d92f799a1c732df0
max <29157840+mmhanson@users.noreply.github.com>
2019-06-28 17:45:49 -0600

    Finished rewriting everything in the dynamic_array header. Now need to do tests, example, and readme.

--- dynamic_array/src/dynamic_array.h
+++ dynamic_array/src/dynamic_array.h
@@ -1,5 +1,57 @@
 /*
- * A generic dynamic array implemented from scratch in C89.
+ * A dynamic array.
+ * See https://en.wikipedia.org/wiki/Dynamic_array for the theory.
+ * Note that this header applies C macros in a complex way. The usage guide
+ * below is meant to be approachable for everyone, but if you're still confused
+ * after reading it then look into parameterized macros and maybe look up some
+ * articles on making generics in C using them if you're confused.
+ *
+ * ====================
+ * ==== How to Use ====
+ * ====================
+
+ * === Declaration ===
+ *   To use the struct and functions defined in this header, you must specify the
+ *   type of data that the dynamic array is to hold (see the README for why). Do
+ *   this by calling the macro `DEFINE_DYNAMIC_ARRAY(T)` where 'T' is the type
+ *   that the dynamic array is to hold (note 'T' must be alphanumerical, pointers must be typecast). This will
+ *   define the struct 'DynamicArray_T' and the operations:
+ *     - darr_T_init(~)
+ *     - darr_T_copy(~)
+ *     - darr_T_insert(~)
+ *     - darr_T_remove(~)
+ *   The reason the 'T' is included in the struct and operation names is so many
+ *   different types of dynamic arrays can be declared and used in the same file.
+ *   Calling the macro `DEFINE_DYNAMIC_ARRAY(T)` more than once does nothing.
+ *
+ * === Declaration Walkthrough ===
+ *   To define a dynamic array holding floats, I must put
+ *   `DEFINE_DYNAMIC_ARRAY(float)` at the top of my source file. After this, the
+     dynamic array type DynamicArray_float is defined as well as the functions:
+ *     - darr_float_init(~)
+ *     - darr_float_copy(~)
+ *     - darr_float_insert(~)
+ *     - darr_float_remove(~)
+ *   If I also want a dynamic array holding ints, I can also put
+ *   `DEFINE_DYNAMIC_ARRAY(int)` at the top of my source file. After that, the
+ *   dynamic array type DynamicArray_int is defined as well as the functions:
+ *     - darr_int_init(~)
+ *     - darr_int_copy(~)
+ *     - darr_int_insert(~)
+ *     - darr_int_remove(~)
+ *   This way, I can have two types of dynamic arrays (holding floats and ints)
+ *   at the same time.
+ *
+ * === Expansion and Contraction ===
+ *   The most important part of a dynamic array is how it expands and contracts.
+ *   Since this header doesn't handle memory, dynamic arrays do not
+ *   automatically expand and contract. Instead, the insert and remove functions
+ *   will indicate that the array is ready for an expansion or contraction with
+ *   their return values. The client of this header then can choose to allocate
+ *   a new array and copy the values over. Copying the values is made easier
+ *   through the `darr_T_copy(~)` function. Exactly when the insert and remove
+ *   function will suggest an expansion or contraction is defined in the
+ *   constants below.
  *
  * Written by Max Hanson, June 2019.
  * Released into the public domain under CC0. See README.md for more details.
@@ -8,43 +60,54 @@
 #ifndef DYNAMIC_ARRAY_H
 #define DYNAMIC_ARRAY_H
 
-#include <stdlib.h>
-
 /*
- * Constants defining when a dynamic array (suggests) expanding/contracting.
+ * Constants defining when a dynamic array suggests expanding/contracting.
  * See usage guide at top for more details.
  */
-static const float EXPANSION_POINT = 1.0; /* load > this -> array expands */
-static const float CONTRACTION_POINT = 0.3; /* load < this -> array contracts */
-/* Expanded capacity = this * old capacity */
-static const float EXPANSION_FACTOR = 2.0;
-/* Contracted capacity = this * old capacity */
-static const float CONTRACTION_FACTOR = 0.5;
+#define EXPANSION_POINT 1.0; /* if load > this, then suggest expansion. */
+#define CONTRACTION_POINT 0.3; /* if load < this, then suggest contraction. */
+
+static float __darr_recalc_load(int size, int capacity);
+
 
 /*
  * Macro to define a dynamic array of type T and its operations.
+ * The `#ifndef` is so no errors will show if a dynamic array of the same type
+ * is declared twice or more.
  *
- * A type parameter 'T' is valid if, and only if:
- *   - It contains no spaces. Note this means pointers must be typecast.
+ * @T: the type parameter. Must be alphanumerical (pointers must be typecast).
  */
-#define DEFINE_DYNAMIC_ARRAY(T)                                                \
+#define DEFINE_DYNAMIC_ARRAY(T) \
+    #ifndef __DYNAMIC_ARRAY_##T \
+    #define __DYNAMIC_ARRAY_##T
+    DEFINE_DYNAMIC_ARRAY_STRUCT(T) \
+    DEFINE_DYNAMIC_ARRAY_INIT(T) \
+    DEFINE_DYNAMIC_ARRAY_COPY(T) \
+    DEFINE_DYNAMIC_ARRAY_INSERT(T) \
+    DEFINE_DYNAMIC_ARRAY_REMOVE(T) \
+    #endif
 
 /*
- * A generic dynamic array.
+ * A dynamic array.
  *
- * Wrapped in a struct for convenience in handling the load/size/capacity.
+ * @load: How full the array is. load = size / capacity.
+ * @size: How many elements of the array are filled.
+ * @capacity: How big the array is.
+ * @array: Pointer to the array of elements.
  */
-#define DEFINE_DYNAMIC_ARRAY_STRUCT(T)                                          \
-    typedef struct DynamicArrayTag_##T                                          \
-    {                                                                           \
-        float load;                                                             \
-        int size;                                                               \
-        int capacity;                                                           \
-        T *array;                                                               \
+#define DEFINE_DYNAMIC_ARRAY_STRUCT(T) \
+    typedef struct DynamicArrayTag_##T \
+    { \
+        float load; \
+        int size; \
+        int capacity; \
+        T *array; \
     } DynamicArray_##T;
 
 /*
- * Initialize a dynamic arra to its default values (zero for all mems).
+ * Initialize a dynamic array to its default values.
+ *
+ * @darr: Pointer to the dynamic array to initialize. All members will be null.
  */
 #define DEFINE_DYNAMIC_ARRAY_INIT(T) \
     static void darr_init(DynamicArray_##T *darr) \
@@ -57,122 +120,94 @@ static const float CONTRACTION_FACTOR = 0.5;
 
 /*
  * Copy the elements of a dynamic array to a new array.
- * @new_array must be >= @darr->size in capacity.
- * Swaps @darr's internal array with new_array once done. Updates capacity and load.
+ * The capacity and load of @self are recalculated, size remains the same.
+ * Caution! The old internal array of @darr is not deallocated!
+ *
+ * @self: Its internal array's elements will be copied to @new_array, then
+ *     @new_array will become its new internal array.
+ * @new_array: Pointer to a new array. @self's internal array elements will be
+ *     copied into this array in order. Caution! must be at least as big in
+ *     capacity as @self's internal array.
+ * @new_cap: The capacity of @new_array. How many elements it can hold. Needed
+ *     to recalculate members of @self.
  */
 #define DEFINE_DYNAMIC_ARRAY_COPY(T) \
-static void darr_copy(DynamicArray_##T *darr, T *new_array, int new_array_capacity)
-{
-    int idx;
-
-    for (idx = 0; idx < darr->size; idx++)
-    {
-        new_array[idx] = darr[idx]
+    static void self_copy(DynamicArray_##T *self, T *new_array, int new_cap) \
+    { \
+        int idx; \
+        \
+        for (idx = 0; idx < self->size; idx++) \
+        { \
+            new_array[idx] = self[idx] \
+        } \
+        \
+        self->array = new_array; \
+        self->capacity = new_cap; \
+        self->load == __self_recalc_load(self->size, self->capacity); \
     }
-
-    darr->array = new_array;
-    darr->capacity = new_array_capacity;
-    darr->load == __darr_recalc_load(darr->size, darr->capacity);
-}
 
 /*
- * Add an element at the i-th index of a dynamic array.
- * @elem will be the new i-th element of the array.
+ * Add an element into a dynamic array.
  *
- * Return 2 if expansion needed (cannot add new elem). 1 if expansion suggested
- * Expansion point reached after insertion.
+ * @self: The dynamic array to insert into. Must have at least one open spot.
+ *     Size will be incremented and load recalculated if the add is successful.
+ * @elem: The element to be added.
+ * @idx: The index to add @elem at. @elem will be the new self->array[i].
+ * @return: 0 if the add was successful. 1 if an expansion is suggested after
+ *     the addition. 2 if the @elem could not be added, array is full.
  */
 #define DEFINE_DYNAMIC_ARRAY_INSERT(T) \
-int darr_##T##_insert(DynamicArray_##T *self, T elem, int i)
-{
-    int idx;
-
-    if (self->size == self->capapcity)
-    {
-        return 2; /* expansion needed */
+    int darr_##T##_insert(DynamicArray_##T *self, T elem, int idx) { \
+        int cursor; \
+        \
+        if (self->size == self->capapcity) \
+        { \
+            return 2; /* expansion needed */ \
+        } \
+        \
+        /* Make hole for the new element and insert it. */ \
+        for (cursor = self->size; cursor > i; cursor--) \
+        { \
+            self->array[cursor - 1] = self->array[cursor]; \
+        } \
+        self->array[idx] = elem; \
+        \
+        self->size++; \
+        self->load = __darr_recalc_load(self->size, self->capacity); \
+        if (load >= EXPANSION_POINT) { \
+            return 1; /* expansion suggested */ \
+        } \
+        return 0; \
     }
-
-    /* Make hole for the new element and insert it. */
-    for (idx = self->size; idx > i; idx--)
-    {
-        self->array[idx - 1] = self->array[idx];
-    }
-    self->array[i] = elem;
-
-    self->size++;
-    self->load = __darr_recalc_load(self->size, self->capacity);
-    if (load >= EXPANSION_POINT)
-    {
-        return 1; /* expansion suggested */
-    }
-}
 
 
 /*
  * Remove the i-th element of a dynamic array.
  *
- * Pair with iteration macro to find and remove an element.
- * Return 1 if contraction suggested. Reached below contraction point.
+ * @self: The dynamic array to remove from. Size will be decremented and load
+ *     will be recalculated.
+ * @idx: The index the element to remove.
+ * @return: 1 if a contraction is suggested after the removal. 0 otherwise.
  */
 #define DEFINE_DYNAMIC_ARRAY_REMOVE(T) \
-int darr_##T##_remove(DynamicArray_##T *self, int idx)
-{
-    int cursor;
-
-    /* Copy all in range (idx..self->size) down one index. Overwrites @idx. */
-    for (cursor = idx + 1; cursor < self->size; cursor++)
-    {
-        self->array[cursor - 1] = self->array[cursor];
+    int darr_##T##_remove(DynamicArray_##T *self, int idx) \
+    { \
+        int cursor; \
+        \
+        /* Copy all in range (idx..self->size) down one. Overwrites @idx. */ \
+        for (cursor = idx + 1; cursor < self->size; cursor++) \
+        { \
+            self->array[cursor - 1] = self->array[cursor]; \
+        } \
+        \
+        self->size--; \
+        self->load = __darr_recalc_load(darr->size, darr->capacity); \
+        if (load <= CONTRACTION_POINT) \
+        { \
+            return 1; /* contraction suggested */ \
+        } \
+        return 0; \
     }
-    self->size--;
-    self->load = __darr_recalc_load(darr->size, darr->capacity);
-    if (load <= CONTRACTION_POINT)
-    {
-        return 1; /* contraction suggested */
-    }
-}
-
-/*
- * Remove the last element in the array.
- * Made a macro to avoid the overhead of an unnecessary function call.
- * Must be a nested macro to put 'T' in the name to fit the rest of the ops.
- *
- * @self must be a pointer to a DynamicArray_T
- */
-#define DEFINE_DYNAMIC_ARRAY_POP(T) \
-    #define darr_##T##_pop(self) darr_##T##_remove(self, self->size - 1)
-
-/*
- * Append an element to the end of a dynamic array.
- * Made a macro to avoid the overhead of an unnecessary function call.
- * Must be a nested macro to put 'T' in the name to fit the rest of the ops.
- *
- * @elem will be the new last element of the array.
- */
-#define DEFINE_DYNAMIC_ARRAY_PUSH(T) \
-    #define darr_##T##_push(self, elem) darr_##T##_insert(self, elem, self->size)
-
-/*
- * A simple macro to iterate over a dynamic array after a certain point.
- * Set @start=0 to iterate over whole array.
- *
- * @darr: The DynamicArray_T to iterate over.
- * @idx: int to track where iteration is in array. Needn't be initialized.
- * @start: int of where to start (and include) iteration.
- */
-#define DARR_FOR_EACH_AFTER(darr, idx, start) \
-    for (idx = start; idx < darr->size; idx++)
-
-/*
- * A simple macro to iterate over a dynamic array between two points.
- *
- * @darr: The DynamicArray_T to iterate over.
- * @idx: int to track where iteration is in array. Needn't be initialized.
- * @start: int of where to start (and include) iteration.
- * @end: int of where to end (not including) iteration.
- */
-#define DARR_FOR_EACH_BETWEEN(darr, start, end) \
-    for (idx = start; idx < end; idx++)
 
 // === HELPER METHODS ===
 
@@ -188,20 +223,5 @@ static float __darr_recalc_load(int size, int capacity)
     return ((float)self->size) / ((float)self->capacity);
 }
 
+
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
