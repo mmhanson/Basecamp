efd8ca5c88307c98bd81cdc426888f08c504b308
max <29157840+mmhanson@users.noreply.github.com>
2019-09-05 16:19:51 -0600

    Added some more tests for buckets.

--- graph/src/graph_tests.c
+++ graph/src/graph_tests.c
@@ -50,18 +50,59 @@ void test_add_first_bucket()
     Graph graph;
     Node node_arr[INIT_SIZE];
     Bucket *bucket;
+    int retval;
 
     graph_init(&graph, node_arr, INIT_SIZE);
     bucket = malloc(sizeof(Bucket));
     graph_bucket_init(bucket);
-    graph_add_bucket(&graph, 0, bucket);
+    retval = graph_add_bucket(&graph, 0, bucket);
 
+    TEST_ASSERT_EQUAL(0, retval);
     TEST_ASSERT_EQUAL(bucket, graph.nodes[0].edges_out);
+}
+
+void test_add_another_bucket()
+{
+    Graph graph;
+    Node node_arr[INIT_SIZE];
+    Bucket *bucket0;
+    Bucket *bucket1;
+    int retval0;
+    int retval1;
+
+    graph_init(&graph, node_arr, INIT_SIZE);
+    bucket0 = malloc(sizeof(Bucket));
+    bucket1 = malloc(sizeof(Bucket));
+    graph_bucket_init(bucket0);
+    graph_bucket_init(bucket1);
+    retval0 = graph_add_bucket(&graph, 0, bucket0);
+    retval1 = graph_add_bucket(&graph, 0, bucket1);
+
+    TEST_ASSERT_EQUAL(0, retval0);
+    TEST_ASSERT_EQUAL(0, retval1);
+    TEST_ASSERT_EQUAL(bucket0, graph.nodes[0].edges_out);
+    TEST_ASSERT_EQUAL(bucket1, graph.nodes[0].edges_out.next);
+}
+
+void test_add_bucket_invalid_node()
+{
+    Graph graph;
+    Node node_arr[INIT_SIZE];
+    Bucket *bucket;
+    int retval;
+
+    graph_init(&graph, node_arr, INIT_SIZE);
+    bucket = malloc(sizeof(Bucket));
+    graph_bucket_init(bucket);
+    retval = graph_add_bucket(&graph, INIT_SIZE + 1, bucket);
+
+    TEST_ASSERT_EQUAL(1, retval);
 }
 
 int main()
 {
     UNITY_BEGIN();
+
 
     /* Init graph. Verify initial values. */
     RUN_TEST(test_init_values);
@@ -69,6 +110,10 @@ int main()
     RUN_TEST(test_init_bucket_values);
     /* Add a bucket to an empty graph. Verify. */
     RUN_TEST(test_add_first_bucket);
+    /* Add two buckets to one node. Verify. */
+    RUN_TEST(test_add_another_bucket);
+    /* Add a bucket to a nonexistent node. Verify error retval. */
+    RUN_TEST(test_add_bucket_invalid_node);
 
 
     UNITY_END();
