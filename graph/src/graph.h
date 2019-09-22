903f191116fcb59ccdb79097f435d548d5b4249a
max <29157840+mmhanson@users.noreply.github.com>
2019-09-22 17:31:58 -0600

    Further refined the graph. I think its in a finished state. Now wrapping up the tests and example.

--- graph/src/graph.h
+++ graph/src/graph.h
@@ -7,18 +7,37 @@
  * specific use case.
  *
  * === How to Use ===
- * The use of this header is fairly straightforward except for the memory
- * allocation. This header performs no memory allocation, so the user is
- * required to allocate and provide the structs for these operations to work
- * with.
- * The nodes in the graph use a linked-list-like structure to keep their edges
- * out. Instead of having one link for each edge out, they are grouped in
- * 'buckets' of a handful of edges and then those buckets are linked together.
- * This is done to alleviate the strain on the heap and to keep the user from
- * having to allocate a new link each time an edge is made.
- * The user will need to allocate these buckets and add them to nodes with the
- * 'add_bucket' operation if there is no space for an edge to be added to a
- * node. The nodes of a newly-initialized graph have no buckets.
+ * There are two main points:
+ *   1. Nodes are accessed by the client via their unique id. All methods that
+ *      receive a node's id will assume that it is valid, and undefined behavior
+ *      will result if it is not.
+ *   2. This header does no memory management. The client will need to provide
+ *      two things for the graph to work: space for the graph to keep its nodes,
+ *      and space for the graph to keep its edges.
+ *      Nodes are kept in an array of nodes which is pointed to by the graph.
+ *      Edges are kept in a linked list stemming from each node, in the manor
+ *      of an adjacency list. The components of this linked list are called
+ *      buckets because they keep a handful of edges. This is done to reduce
+ *      indirection during runtime. The client will need to provide nodes with
+ *      buckets via the 'add_bucket' operation, otherwise the 'add_edge'
+ *      operation will fail.
+ *
+ * === How to Modify ===
+ * This header was desigend to be extensible and basic, not a catch-all graphing
+ * library. Below I've written some of the ways I left the header open for
+ * extensions.
+ *   - Graph nodes are consolidated to their own struct, making it simple to add
+ *     or modify information pertaining to individual nodes. Node ids can be
+ *     changed to be more meaningful to their application, eg names of cities.
+ *     And more information can be added to nodes, eg their degree.
+ *   - The graph can be made undirected with a simple extension of the add_edge
+ *     operation to add an edge to both nodes in question.
+ *   - The graph can be made weighted by recording weights next to edges in the
+ *     nodes buckets.
+ *   - If graphs are very sparse, an array of edges can be kept in the nodes
+ *     themselves, reducing the memory management done by the user. You could
+ *     even embed the array of nodes in the graph struct to elminiate the heap
+ *     entirely.
  *
  * Written by Max Hanson, September 2019.
  * Released into the public domain under CC0. See README.md for more details.
@@ -27,22 +46,22 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-
-/* How many edges out per bucket. */
-#define BUCKET_SIZE 10
-
 typedef struct BucketTag Bucket;
 typedef struct NodeTag Node;
 typedef struct GraphTag Graph;
 
+/*  how many edges out per bucket  */
+#define BUCKET_SIZE 10
+
+static Node *graph_find_node_by_id(Graph *graph, int node_id);
+static Node **graph_find_edge(Graph *graph, int from_id, int to_id);
+static Node **graph_find_empty_edge(Graph *graph, int node_id);
+static Node **graph_find_pointer(Graph *graph, Node *node_from, Node *node_to);
+
 /*
- * A bucket of edges out of a vertex.
- * Each node in the graph has a linked list of outgoing edges. These edges are
- * stored in 'buckets' of a few edges out (the exact number defined in
- * BUCKET_SIZE) and links to the next. This reduces indirection and the load on
- * the heap.
+ * A bucket of edges out of a node.
  *
- * @adj_nodes: Edges out of the node.
+ * @adj_nodes: Array of edges out of the node.
  * @next: The next bucket. Null if this is the last bucket.
  */
 struct BucketTag
@@ -83,15 +102,14 @@ struct GraphTag
 /*
  * Initialize a graph.
  * @graph will be initialized to use @node_arr for its node storage and its
- * attributes will be initialized:
- *   size: @node_arr_size
- *   num_nodes, num_edges: 0
- *   nodes: Each node in the array will get an id equal to their index and its
+ * attributes will be initialized to:
+ *   - size: @node_arr_size
+ *   - num_nodes, num_edges: 0
+ *   - nodes: Each node in the array will get an id equal to their index and its
  *     edges_out attribute will be null.
  * @graph's previous 'nodes' attribute will not be modified, so this function
- * can be used to re-initialize a graph to expand/contract it.
+ * can also be used to re-initialize a graph to expand/contract it.
  *
- * @graph: The graph to initialize.
  * @node_arr: An array for the graph to keep its nodes in.
  * @node_arr_size: The size of @node_arr.
  */
@@ -112,40 +130,43 @@ static void graph_init(Graph *graph, Node* node_arr, int node_arr_size)
 }
 
 /*
- * Initialize and add a bucket to a node's edges-out-list.
+ * Initialize and add a bucket to a node's edge list.
  *
- * @graph: The graph.
  * @node_id: Id of the node to add @bucket.
  * @bucket: pointer to bucket object to use for @node. Assumed to be just
  *   allocated. All fields will be initialized to null. Will be linked into the
  *   node's list of buckets as the last bucket.
- * @return: 0 if the bucket was added. -1 if the node could not be found.
  */
-static int graph_add_bucket(Graph *graph, int node_id, Bucket* bucket)
+static void graph_add_bucket(Graph *graph, int node_id, Bucket* bucket)
 {
     int idx;
-    Bucket **link_cursor;
-    Node *node; /* Node to add bucket to. */
+    Bucket *cursor;
+    Node *node;
 
-    node = graph_get_node_by_id(graph, node_id);
-    if (node == 0)
-    {
-        return -1;
-    }
+    node = graph_find_node_by_id(graph, node_id);
 
+    /*  initialize the bucket  */
     bucket->next = 0;
     for (idx = 0; idx < BUCKET_SIZE; idx++)
     {
         bucket->adj_nodes[idx] = 0;
     }
 
-    /* Find the last link (Bucket*) of the node's bucket chain. */
-    link_cursor = &(node->edges_out);
-    while ( (*link_cursor) != 0 )
+    /*  start a bucket chain, if one doesnt exist  */
+    if (node->edges_out == 0)
     {
-        link_cursor = (*link_cursor)->next;
+        node->edges_out = bucket;
     }
-    (*link_cursor) = bucket; /* Add new bucket onto chain. */
+    /*  otherwise, add the bucket to the end of the chain  */
+    else
+    {
+        cursor = node->edges_out;
+        while (cursor->next != 0)
+        {
+            cursor = cursor->next;
+        }
+        cursor->next = bucket;
+    }
 
     return 0;
 }
@@ -153,168 +174,182 @@ static int graph_add_bucket(Graph *graph, int node_id, Bucket* bucket)
 /*
  * Add an edge to a graph.
  *
- * @graph: the graph to add the edge to.
- * @from_id: the id of the node the edge starts at.
- * @to_id: the id of the node the edge ends at.
- * @return: 0 if the edge was added, -1 if there wasn't enough space for it,
- *   and -2 if either node id is invalid.
+ * @from_id: The id of the node the edge starts at. Assumed to be valid.
+ * @to_id: The id of the node the edge ends at. Assumed to be valid.
+ * @return: 0 if the edge was added, 1 if there wasn't enough space for it.
  */
 static int graph_add_edge(Graph *graph, int from_id, int to_id)
 {
     Node *node_to;
-    Node **edge;
-    int status;
+    Node **edge_spot;
 
-    /* Find first null pointer in @from_ids list of edges out. */
-    status = graph_find_edge(graph, from_id, 0, edge);
-    if (status == 1)
+    edge_spot = graph_find_empty_edge(graph, from_id);
+    if (edge_spot == 0)
     {
-        return -1; /* No space for a new edge. */
-    }
-    if (status == -1)
-    {
-        return -2; /* @from_id is invalid. */
+        /*  no empty edges  */
+        return 1;
     }
 
-    /* Find what to point to edge to. */
-    node_to = graph_get_node_by_id(graph, to_id);
-    if (node_to == 0)
-    {
-        return -2; /* @to_id is invald. */
-    }
+    node_to = graph_find_node_by_id(graph, to_id);
+    (*edge) = node_to;
 
-    (*edge) = node; /* Add the edge. */
     return 0;
 }
 
 /*
  * Remove an edge from a graph.
  *
- * @graph: the graph to remove the edge from.
- * @from_id: the id of the node the edge starts at.
- * @to_id: the id of the node the edge ends at.
- * @return: 0 if the edge was deleted. 1 if the edge does not exist. -1 if
- *   either node id is invalid.
+ * @from_id: The id of the node the edge starts at. Assumed to be valid.
+ * @to_id: The id of the node the edge ends at. Assumed to be valid.
  */
-static int graph_del_edge(Graph *graph, int from_id, int to_id)
+static void graph_del_edge(Graph *graph, int from_id, int to_id)
 {
     Node **edge;
-    int status;
 
-    /* Find first null pointer in @from_ids list of edges out. */
-    status = graph_find_edge(graph, from_id, to_id, edge);
-    if (status == 1)
+    /*  find first null pointer in @from_ids list of edges out  */
+    edge = graph_find_edge(graph, from_id, to_id);
+
+    if (edge == 0)
     {
-        return 1; /* Edge does not exist. */
-    }
-    if (status == -1)
-    {
-        return -1; /* A node is invalid. */
+        /*  edge doesnt exist, consider it deleted  */
+        return;
     }
 
-    (*edge) = 0; /* Delete the edge. */
-    return 0;
+    /*  delete the edge  */
+    (*edge) = 0;
 }
 
 /*
  * Determine if there is an edge in a graph.
  *
- * @graph: the graph to check for the edge.
- * @from_id: the id of the node the edge starts at.
- * @to_id: the id of the node the edge ends at.
- * @return: 0 if the graph has the edge. 1 if not. -1 if either node is is
- *   invalid.
+ * @from_id: The id of the node the edge starts at. Assumed to be valid.
+ * @to_id: The id of the node the edge ends at. Assumed to be valid.
+ * @return: 0 if the graph has the edge. 1 if it doesnt.
  */
 static int graph_has_edge(Graph *graph, int from_id, int to_id)
 {
-    Node **edge;
+    int status;
 
-    /* Return statuses match up. */
-    return graph_find_edge(graph, from_id, to_id, edge);
+    status = graph_find_edge(graph, from_id, to_id);
+
+    if (status == 0)
+    {
+        return status;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+/*
+ * Determine if a node id is valid.
+ *
+ * @return: Bool. 1 if the node id is valid, 0 if not.
+ */
+static int graph_node_id_valid(Graph *graph, int node_id)
+{
+    /*  a nodes id is just its index  */
+    if (node_id < 0 || node_id >= graph->size)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 
 /* === HELPER FUNCTIONS === */
 
 /*
- * Get a node using its unique identifier.
- * Modify this method if you want to change how nodes are identified.
+ * Find a node using its unique identifier.
  *
- * @graph: The graph with the node to find.
- * @node_id: The unique identifier for the node.
- * @return: Pointer to the node, or null (0) if the node cannot be found.
+ * @node_id: The unique identifier for the node. Assumed to be valid.
+ * @return: Pointer to the corresponding node.
+ * @return: Pointer to the node, or null if the node cannot be found.
  */
-static Node *graph_get_node_by_id(Graph *graph, int node_id)
+static Node *graph_find_node_by_id(Graph *graph, int node_id)
 {
-    /* A nodes id is just its index. */
-    if (node_id < 0 || node_id >= graph->size)
-    {
-        return 0;
-    }
-
+    /*  a nodes id is just its index  */
     return &graph->nodes[node_id];
 }
 
 /*
  * Find the edge in a graph.
- * Note that both @from_id must be a valid id. @to_id must either be a valid id
- * or -1 (see below).
  *
- * @graph: The graph to find the edge in.
- * @from_id: Id of the node the edge starts at. Must be a valid node id.
- * @to_id: Id of he node the edge ends at. Must be a valid node id or -1.
- *   Provide a value of -1 to find the first empty spot to put a new edge in the
- *   graph. Will return 1 if there is no empty spots to put a new edge in.
- * @edge_ptr: Will point to the found edge if @return is 0. Otherwise
- *   unmodified. This will point to the spot in @from_id's list of edges out
- *   that points to @to_id.
- * @return: 0 if the edge was found and @edge_ptr set. 1 if the node is not
- *   found and -1 if either node id is invalid.
+ * @from_id: Id of the node the edge starts at. Assumed to be valid.
+ * @to_id: Id of he node the edge ends at. Assumed to be valid.
+ * @return:
+ *   1. A pointer to the edge. Points to the spot in @from_id's list of edges
+ *      out that points to @to_id.
+ *   2. Null if the edge doesn't exist or either node id is invalid.
  */
-static int graph_find_edge(Graph *graph, int from_id, int to_id, Node **edge_ptr)
+static Node **graph_find_edge(Graph *graph, int from_id, int to_id)
 {
     Node *node_to;
     Node *node_from;
 
-    node_from = graph_get_node_by_id(graph, from_id);
-    if (node_from == 0)
-    {
-        return -1; /* Invalid @from_id. */
-    }
+    node_to = graph_find_node_by_id(graph, to_id);
+    node_from = graph_find_node_by_id(graph, from_id);
 
-    if (to_id == -1)
-    {
-        node_to = 0; /* Looking for empty spot in @node_from. */
-    }
-    else
-    {
-        /* Looking for an actual edge. */
-        node_to = graph_get_node_by_id(graph, to_id);
-        if (node_to == 0)
-        {
-            return -1; /* Invalid @to_id. */
-        }
-    }
+    return graph_find_pointer(graph, node_from, node_to);
+}
 
+/*
+ * Find the first empty spot to put a new edge in a graph.
+ *
+ * @node_id: Id of the node to find the empty edge in. Assumed to be valid.
+ * @return:
+ *   1. A pointer to the emtpy edge. Points to the spot in @node_id's list of
+ *      edges out that is empty.
+ *   2. Null if there is no empty edges
+ */
+static Node **graph_find_empty_edge(Graph *graph, int node_id)
+{
+    Node *node;
+
+    node = graph_find_node_by_id(graph, node_id);
+
+    /*  find first null pointer in @node's list of edges out  */
+    return graph_find_pointer(graph, node, 0);
+}
+
+/*
+ * Find a specific pointer in a node's list of edges.
+ *
+ * @node_from: The node whose list of edges out to find the pointer in. Assumed
+ *   to be non-null.
+ * @node_to: The target pointer to find in @node_from's list of edges out. Can
+ *   be null, in which case it will find the first null edge.
+ * @return:
+ *   1. A pointer to the spot that points to @node_to.
+ *   2. Null, if the pointer doesn't exist.
+ */
+static Node **graph_find_pointer(Graph *graph, Node *node_from, Node *node_to)
+{
+    int idx;
+    Bucket *cursor;
+    Node **adj_nodes;
+
+    /*  iterate through each bucket  */
     cursor = node_from->edges_out;
-    /* Iterate through each bucket. */
     while (cursor != 0)
     {
-        /* Iterate through each spot in the bucket. */
+        /*  iterate through each spot in the bucket  */
         for (idx = 0; idx < BUCKET_SIZE; idx++)
         {
-            if (cursor->adj_nodes[idx] == node_to)
+            adj_nodes = cursor->adj_nodes;
+            if (adj_nodes[idx] == node_to)
             {
-                edge_ptr = &(cursor->adj_nodes[idx]);
-                return 0;
+                /*  pointer is found  */
+                return adj_nodes[idx]
             }
         }
 
-        /* Go to next bucket. */
         cursor = cursor->next;
     }
-    /* Edge not found. */
-    return 1;
+
+    /*  edge not found  */
+    return 0;
 }
 
 
