8023470c31c5248105c6f07f9ecbd425f589bef3
max <29157840+mmhanson@users.noreply.github.com>
2019-09-22 17:56:08 -0600

    Updated the tests to reflect the revamped graph header.

--- graph/src/graph_tests.c
+++ graph/src/graph_tests.c
@@ -31,20 +31,17 @@ void test_init_values()
     }
 }
 
-void test_add_first_bucket()
+void test_bucket_init()
 {
     Graph graph;
     Node node_arr[INIT_SIZE];
     Bucket *bucket;
-    int retval;
     int idx;
 
     graph_init(&graph, node_arr, INIT_SIZE);
     bucket = malloc(sizeof(Bucket));
-    retval = graph_add_bucket(&graph, 0, bucket);
+    graph_add_bucket(&graph, 0, bucket);
 
-    TEST_ASSERT_EQUAL(0, retval);
-    TEST_ASSERT_EQUAL(bucket, graph.nodes[0].edges_out);
     /* Validate bucket attributes. */
     for (idx = 0; idx < BUCKET_SIZE; idx++)
     {
@@ -53,39 +50,35 @@ void test_add_first_bucket()
     TEST_ASSERT_EQUAL(NULL, bucket->next);
 }
 
+void test_add_first_bucket()
+{
+    Graph graph;
+    Node node_arr[INIT_SIZE];
+    Bucket *bucket;
+    int idx;
+
+    graph_init(&graph, node_arr, INIT_SIZE);
+    bucket = malloc(sizeof(Bucket));
+    graph_add_bucket(&graph, 0, bucket);
+
+    TEST_ASSERT_EQUAL(bucket, graph.nodes[0].edges_out);
+}
+
 void test_add_another_bucket()
 {
     Graph graph;
     Node node_arr[INIT_SIZE];
     Bucket *bucket0;
     Bucket *bucket1;
-    int retval0;
-    int retval1;
 
     graph_init(&graph, node_arr, INIT_SIZE);
     bucket0 = malloc(sizeof(Bucket));
     bucket1 = malloc(sizeof(Bucket));
-    retval0 = graph_add_bucket(&graph, 0, bucket0);
-    retval1 = graph_add_bucket(&graph, 0, bucket1);
+    graph_add_bucket(&graph, 0, bucket0);
+    graph_add_bucket(&graph, 0, bucket1);
 
-    TEST_ASSERT_EQUAL(0, retval0);
-    TEST_ASSERT_EQUAL(0, retval1);
     TEST_ASSERT_EQUAL(bucket0, graph.nodes[0].edges_out);
     TEST_ASSERT_EQUAL(bucket1, graph.nodes[0].edges_out->next);
-}
-
-void test_add_bucket_invalid_node()
-{
-    Graph graph;
-    Node node_arr[INIT_SIZE];
-    Bucket *bucket;
-    int retval;
-
-    graph_init(&graph, node_arr, INIT_SIZE);
-    bucket = malloc(sizeof(Bucket));
-    retval = graph_add_bucket(&graph, INIT_SIZE + 1, bucket);
-
-    TEST_ASSERT_EQUAL(1, retval);
 }
 
 void test_basic_add_edge()
@@ -163,35 +156,6 @@ void test_add_couple_edges()
     TEST_ASSERT_EQUAL(1, graph_has_edge(&graph, 9, 8));
 }
 
-void test_add_invalid_edges()
-{
-    Graph graph;
-    Node node_arr[INIT_SIZE];
-    Bucket *bucket;
-    int idx;
-    int retval;
-
-    /* Init graph with one bucket for each node */
-    graph_init(&graph, node_arr, INIT_SIZE);
-    for (idx = 0; idx < INIT_SIZE; idx++)
-    {
-        bucket = malloc(sizeof(Bucket));
-        graph_add_bucket(&graph, idx, bucket);
-    }
-
-    /* Second node invalid. */
-    retval = graph_add_edge(&graph, 0, INIT_SIZE + 1);
-    TEST_ASSERT_EQUAL(-2, retval);
-
-    /* First node invalid. */
-    retval = graph_add_edge(&graph, INIT_SIZE + 1, 0);
-    TEST_ASSERT_EQUAL(-2, retval);
-
-    /* Both nodes invalid. */
-    retval = graph_add_edge(&graph, -5, INIT_SIZE + 1);
-    TEST_ASSERT_EQUAL(-2, retval);
-}
-
 void test_add_edge_no_space()
 {
     Graph graph;
@@ -202,7 +166,7 @@ void test_add_edge_no_space()
     graph_init(&graph, node_arr, INIT_SIZE);
     retval = graph_add_edge(&graph, 0, 1);
 
-    TEST_ASSERT_EQUAL(-1, retval);
+    TEST_ASSERT_EQUAL(1, retval);
 }
 
 void test_basic_del_edge()
@@ -258,89 +222,6 @@ void test_del_couple_edges()
     TEST_ASSERT_EQUAL(0, graph_has_edge(&graph, 3, 4));
 }
 
-void test_del_nonexistent_edge()
-{
-    Graph graph;
-    Node node_arr[INIT_SIZE];
-    Bucket *bucket;
-    int idx;
-    int retval;
-
-    /* Init graph with one bucket for each node */
-    graph_init(&graph, node_arr, INIT_SIZE);
-    for (idx = 0; idx < INIT_SIZE; idx++)
-    {
-        bucket = malloc(sizeof(Bucket));
-        graph_add_bucket(&graph, idx, bucket);
-    }
-
-    retval = graph_del_edge(&graph, 0, 1);
-
-    TEST_ASSERT_EQUAL(1, retval);
-}
-
-void test_del_invalid_edges()
-{
-    Graph graph;
-    Node node_arr[INIT_SIZE];
-    Bucket *bucket;
-    int idx;
-    int retval;
-
-    /* Init graph with one bucket for each node */
-    graph_init(&graph, node_arr, INIT_SIZE);
-    for (idx = 0; idx < INIT_SIZE; idx++)
-    {
-        bucket = malloc(sizeof(Bucket));
-        graph_add_bucket(&graph, idx, bucket);
-    }
-
-    /* First node invalid. */
-    retval = graph_del_edge(&graph, INIT_SIZE + 1, 0);
-    TEST_ASSERT_EQUAL(-1, retval);
-    
-    /* Second node invalid. */
-    retval = graph_del_edge(&graph, 0, INIT_SIZE + 1);
-    TEST_ASSERT_EQUAL(-1, retval);
-
-    /* Both node invalid. */
-    retval = graph_del_edge(&graph, INIT_SIZE + 1, -1);
-    TEST_ASSERT_EQUAL(-1, retval);
-}
-
-void test_del_edges_populated_nodes()
-{
-    Graph graph;
-    Node node_arr[INIT_SIZE];
-    Bucket *bucket;
-    int idx;
-    int retval;
-
-    /* Init graph with one bucket for each node */
-    graph_init(&graph, node_arr, INIT_SIZE);
-    for (idx = 0; idx < INIT_SIZE; idx++)
-    {
-        bucket = malloc(sizeof(Bucket));
-        graph_add_bucket(&graph, idx, bucket);
-    }
-
-    graph_add_edge(&graph, 0, 1);
-    graph_add_edge(&graph, 8, 9);
-    graph_add_edge(&graph, 7, 2);
-    graph_add_edge(&graph, 2, 3);
-    graph_add_edge(&graph, 4, 6);
-    graph_add_edge(&graph, 4, 5);
-
-    retval = graph_del_edge(&graph, 3, 4);
-    TEST_ASSERT_EQUAL(1, retval);
-
-    retval = graph_del_edge(&graph, 6, 8);
-    TEST_ASSERT_EQUAL(1, retval);
-
-    retval = graph_del_edge(&graph, 2, 4);
-    TEST_ASSERT_EQUAL(1, retval);
-}
-
 void test_has_edge_empty_graph()
 {
     Graph graph;
@@ -381,79 +262,39 @@ void test_basic_not_has_edge()
     TEST_ASSERT_EQUAL(1, retval);
 }
 
-void test_has_edge_invalid_nodes()
-{
-    Graph graph;
-    Node node_arr[INIT_SIZE];
-    Bucket *bucket;
-    int idx;
-    int retval;
-
-    /* Init graph with one bucket for each node */
-    graph_init(&graph, node_arr, INIT_SIZE);
-    for (idx = 0; idx < INIT_SIZE; idx++)
-    {
-        bucket = malloc(sizeof(Bucket));
-        graph_add_bucket(&graph, idx, bucket);
-    }
-
-    /* Second invalid. */
-    retval = graph_has_edge(&graph, 0, INIT_SIZE + 1);
-    TEST_ASSERT_EQUAL(1, retval);
-
-    /* First invalid */
-    retval = graph_has_edge(&graph, INIT_SIZE + 1, 4);
-    TEST_ASSERT_EQUAL(1, retval);
-
-    /* Both invalid. */
-    retval = graph_has_edge(&graph, -1, INIT_SIZE + 1);
-    TEST_ASSERT_EQUAL(1, retval);
-}
-
 int main()
 {
     UNITY_BEGIN();
 
 
-    /* Init graph. Verify initial values. */
+    /*   verify initial values of a graph  */
     RUN_TEST(test_init_values);
 
-    /* Add a bucket to an empty graph. Verify. Verify attr init'n of bucket. */
+    /*  add a bucket to an empty graph, verify bucket initialization  */
+    RUN_TEST(test_bucket_init);
+    /*  add a bucket to an empty graph, verify addition  */
     RUN_TEST(test_add_first_bucket);
-    /* Add two buckets to one node. Verify. */
+    /*  add two buckets to one node  */
     RUN_TEST(test_add_another_bucket);
-    /* Add a bucket to a nonexistent node. Verify error retval. */
-    RUN_TEST(test_add_bucket_invalid_node);
 
-    /* Add a single edge between 0th and 1st nodes. Verify. */
+    /*  add a single edge between first two nodes, verify edge  */
     RUN_TEST(test_basic_add_edge);
-    /* Add a double edge between two nodes. Verify. */
+    /*  add a double edge between first two ndoes, verify edges  */
     RUN_TEST(test_double_edge);
-    /* Add a few edges between nodes. Verify. */
+    /*  add a few edges between nodes  */
     RUN_TEST(test_add_couple_edges);
-    /* Test adding invalid edges of all kinds. Verify retvals. */
-    RUN_TEST(test_add_invalid_edges);
-    /* Test adding an edge to a node with no bucket space. Verify error. */
+    /*  add an edge to a node with no space for an edge, verify the error  */
     RUN_TEST(test_add_edge_no_space);
 
-    /* Add an edge and delete it. Verify removal. */
+    /*  add a edge and delete it, verify removal  */
     RUN_TEST(test_basic_del_edge);
-    /* Add some edges. Delete some edges (not all). Verify. */
+    /*  add a few edges, delete some but not all, verify results  */
     RUN_TEST(test_del_couple_edges);
-    /* Delete an edge that doesn't exist. Verify error. */
-    RUN_TEST(test_del_nonexistent_edge);
-    /* Try to delete an edge between invalid node ids. Verify errors. */
-    RUN_TEST(test_del_invalid_edges);
-    /* Delete edges from nodes that have edges to other nodes, but not to the
-       node we're trying to delete the edge from. Verify. */
-    RUN_TEST(test_del_edges_populated_nodes);
 
-    /* Test if an empty graph (nodes but no buckets) has an edge. */
+    /*  test that an empty graph doesnt have an edges  */
     RUN_TEST(test_has_edge_empty_graph);
-    /* Test a nonempty graph (nodes have buckets) for nonexistent edges. */
+    /*  test a basic graph for nonexistent edges  */
     RUN_TEST(test_basic_not_has_edge);
-    /* Test a nonemtpy graph for edges of invalid nodes. */
-    RUN_TEST(test_has_edge_invalid_nodes);
 
 
     UNITY_END();
